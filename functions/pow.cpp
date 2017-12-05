828d295304ceaa7da295d4df49e7fb7fb3337fc2
Jear-Bear <jperlmutter1@gmail.com>
2017-12-05 09:54:20 -0600

    Update pow.cpp

--- functions/pow.cpp
+++ functions/pow.cpp
@@ -1,4 +1,11 @@
 #include "complex.h";
 //Author: Jared
-//Purpose: Takes two arguments: pow(b, exp). It raises b to the
-power of exp.
+//Purpose: Takes two arguments: pow(b, exp). It raises b to the power of exp.
+void pow (const complex &b, int exp)
+{
+  for (int i = 0; i < exp; i++)
+  {
+    real(b) *= real(b);
+    imag(b) *= imag(b);
+  }
+}
