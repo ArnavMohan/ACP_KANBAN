79f0d736ecbf4c9b10a9aaf38104605edd8998ce
rdchan <30301489+rdchan@users.noreply.github.com>
2017-12-05 09:41:15 -0600

    Create conj.cpp

---
+++ functions/conj.cpp
@@ -0,0 +1,13 @@
+#include "complex.h"
+
+//Author: Rishi
+//Function: conjugate
+//Purpose: Returns the conjugate of the input complex number. 
+//The conjugate is just the regular with different signs on the real part and the imaginary part
+
+using namespace std;
+
+complex conj(const complex input_complex){
+  complex final = new complex(-1*real(input_complex), -1*imag(input_complex));
+  return final;
+}
