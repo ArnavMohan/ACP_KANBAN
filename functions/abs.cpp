ccfb91f941c42cda5db78721c856f34facb3316f
Jear-Bear <jperlmutter1@gmail.com>
2017-12-05 09:32:25 -0600

    Update abs.cpp

--- functions/abs.cpp
+++ functions/abs.cpp
@@ -1,2 +1,5 @@
-
+#include complex.h;
+//Author: Jared
+//Operator: -
+//Purpose: Subtracts two complex numbers and returns a complex data type with a real and imaginary value.
 double abs(const complex)
