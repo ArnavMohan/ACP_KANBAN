369fe7fe93a2fd4272b29db8f30986eff5c55434
Jear-Bear <jperlmutter1@gmail.com>
2017-12-05 09:22:58 -0600

    Operator overload of Minus

--- operators/minus.cpp
+++ operators/minus.cpp
@@ -1,3 +1,12 @@
 //Author: Jared
 //Operator: -
-//Purpose: 
+//Purpose: Subtracts two complex numbers and returns a complex data type with a real and imaginary value.
+complex operator-(const complex &lhs, const complex &rhs)
+{
+      double resulting_real = real(&lhs) - real(&rhs);
+      double resulting_imag = imag(&lhs) - imag(&rhs);
+      //new complex type initialized with real and imaginary parts
+      //return that complex number
+      complex final = new conplex(resulting_real, resulting_imag);
+      return final;
+}
