3fe6ba38a8b3ec1e0e213a7077d553a51ba1bf43
Abhijay Saini <abhijay.saini@gmail.com>
2017-12-07 09:26:08 -0600

    plus updated

--- operators/plus.cpp
+++ operators/plus.cpp
@@ -1,3 +1,19 @@
-//Author: Jared
+#include "complex.h"
+//Author: Abhijay
 //Operator: +
-//Purpose: 
+//Purpose: Adds two complex numbers and returns a complex data type with a real and imaginary value.
+complex operator +(const complex &lhs, const complex &rhs)
+{
+      double resulting_real = real(&lhs) + real(&rhs);
+      double resulting_imag = imag(&lhs) + imag(&rhs);
+      //new complex type initialized with real and imaginary parts
+      //return that complex number
+      complex final = new complex(resulting_real, resulting_imag);
+      return final;
+}
+
+void operator +=(const complex &lhs, const complex &rhs)
+{
+      complex final = &lhs + &rhs;
+      lhs = final;
+}
