1f117e178fd16dc6232b9979a097744b0b09006d
bross18 <benjiross01@gmail.com>
2017-12-07 09:15:22 -0600

    Update divide.cpp

--- operators/divide.cpp
+++ operators/divide.cpp
@@ -13,5 +13,6 @@ complex operator/(const complex &lhs, const complex &rhs){
 }
 
 void operator/=(const complex &lhs, const complex &rhs){
-    
+      complex final = &lhs / &rhs;
+      lhs = final;
 }
