f733d66574538f05e462f61230da76300424985e
Arnav Mohan <arnav.mohan@gmail.com>
2017-12-07 08:53:20 -0600

    ...

--- notequal.cpp
+++
@@ -1,8 +0,0 @@
-//Author: Arnav
-//Operator: !=
-//Purpose: Check nonequality between 2 complex numbers
-#include "complex.h"
-using namespace std;
-boolean operator!=(const complex &lhs, const complex &rhs){
-	return !(lsh == rhs);
-}
