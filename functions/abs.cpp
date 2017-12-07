3b7bb95d2773c7475e4bfa9e1778726dedb3beea
Jear-Bear <jperlmutter1@gmail.com>
2017-12-07 09:13:15 -0600

    Update abs.cpp

--- functions/abs.cpp
+++ functions/abs.cpp
@@ -1,9 +1,9 @@
 #include "../complex.h";
 //Author: Jared
 //Purpose: Returns the magnitude of a complex number.
-double abs(const complex &comp)
+double abs(const complex comp)
 {
-  double r = real(pow(&comp, 2));
-  double i = imag(pow(&comp, 2));
+  double r = real(pow(comp, 2));
+  double i = imag(pow(comp, 2));
   return real(sqrt(r + i));
 }
