739b2558bba71ebe5f5196a44aa21cd0b8b6b2cf
Jear-Bear <jperlmutter1@gmail.com>
2017-12-07 09:22:43 -0600

    Update abs.cpp

--- functions/abs.cpp
+++ functions/abs.cpp
@@ -3,7 +3,7 @@
 //Purpose: Returns the magnitude of a complex number.
 double abs(const complex comp)
 {
-  double r = real(pow(comp, 2));
-  double i = imag(pow(comp, 2));
-  return real(sqrt(r + i));
+  double r = complex::real(pow(comp, 2));
+  double i = complex::imag(pow(comp, 2));
+  return complex::real(complex::sqrt(r + i));
 }
