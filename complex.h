bc7ff08611ec18461a31b24a7d65be6dd3d253ac
PokeMaster100 <rishirules01@gmail.com>
2017-12-07 23:52:39 -0600

    beautified header, added power function and power directory

--- complex.h
+++ complex.h
@@ -1,85 +1,86 @@
 class complex 
 {
     double re, im;
+
+    ////////////////
+    //constructors//
+    ////////////////
+
     complex::complex() {re=0.0; im=0.0;}
-    complex::complex(double r, double i = 0.0) {re=r; im=i;}
+    complex::complex(double r, double i) {re=r; im=i;}
+    complex::complex(double r){re=r, im=0}
 
-	
-    complex pow(const complex b, const complex exp);
-
+    //////////////////	
+    //base functions//
+    //////////////////
+    
+    double real(const complex&);
+    double imag(const complex&);
+    double abs(const complex);    
     double norm (const complex);
-
-    double polar(double mag, double ang = 0.0);
-
-    complex log(const complex);
-
-    double arg(const complex);
-
-    complex exp(const complex);
-
-    complex log10(const complex);
-
+    complex conj(const complex);
     complex sqrt(const complex);
 
-    double abs(const complex);
+    //////////////////
+    //trig functions//
+    //////////////////
 
+    complex sin(const complex);
     complex cos(const complex);
-
-    complex acos(const complex);
-
-    complex pow(const complex b, int exp);
-
-    complex asin(const complex);
-
-    complex pow(dobule b, const complex exp);
-
     complex tan(const complex);
+
+    complex cosh(const complex);
+    complex sinh(const complex);
 
     complex atan(const complex);
 
+    ////////////////////////////
+    //exponentiation functions//
+    ////////////////////////////
+
+    complex pow(const complex b, const complex exp);
+    complex pow(double b, const complex exp);
     complex pow(const complex b, double exp);
+    complex pow(const complex b, int exp);
 
-    complex sin(const complex);
+    complex exp(const complex);
+    
+    /////////////////////////
+    //logarithmic functions//
+    /////////////////////////
 
-    double real(const complex&);
+    complex log(const complex);
+    complex log10(const complex);
 
-    double imag(const complex&);
+    /////////////////////
+    //angular functions//
+    /////////////////////
 
-    complex conj(const complex);
+    double polar(double mag, double ang = 0.0);
+    double arg(const complex);
 
-    complex operator/(const complex &lhs, const complex &rhs);
-
-    void operator/=(const complex &lhs, const complex &rhs);
-
-    complex exp(const complex& x)
+    ////////////////////////
+    //arithmetic operators//
+    ////////////////////////
+  
+    complex operator+(const complex &lhs, const complex &rhs);
+    void operator +=(const complex &lhs, const complex &rhs);
 
     complex operator-(const complex &lhs, const complex &rhs);
-
     void operator-=(const complex &lhs, const complex &rhs);
 
     complex operator*(const complex &lhs, const complex &rhs);
-
     void operator*=(const complex &lhs, const complex &rhs);
 
-    complex operator+(const complex &lhs, const complex &rhs);
+    complex operator/(const complex &lhs, const complex &rhs);
+    void operator/=(const complex &lhs, const complex &rhs);
 
-    void operator +=(const complex &lhs, const complex &rhs);
-
-    boolean operator==(const complex &lhs, const complex &rhs);
-
-    boolean operator!=(const complex &lhs, const complex &rhs);
-
-
-
-
-
-   
+    /////////////////////
+    //logical operators//
+    /////////////////////
     
-
-
-
-
-
-        
+    boolean operator==(const complex &lhs, const complex &rhs);
+    boolean operator!=(const complex &lhs, const complex &rhs);
+       
 };
 
---
+++ functions/pow/doubtocomp.cpp
@@ -0,0 +1,19 @@
+#include "../complex.h"
+#include <math.h>
+//Author: Rishi
+//Function: pow
+//Purpose: Raises the double to the complex number
+
+using namespace std;
+
+complex pow(double b, const complex exp){
+	//computes the exponentiation by using b^(exp_re + exp_im i) = 
+			//b^exp_re * (cos( exp_im*ln(b) )  +  i sin( exp_im*ln(b) )
+			//double times a complex number, distribute
+	double distribution_scalar = pow(b, complex::real(exp)); //uses pow(double b, double exp) from math.h 
+	double real_component = distribution_scalar * cos( complex::imag(exp) * log(b) );  //uses log as ln from math.h
+	double imag_component = distribution_scalar * sin( complex::imag(exp) * log(b) );  //uses log as ln from math.h
+	
+	complex final_value = new complex(real_component, imag_component);
+	return final_value;
+}
