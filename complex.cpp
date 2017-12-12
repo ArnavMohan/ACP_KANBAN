cb61b0d8a97d967e5b1037546ff0331afcbee35b
Arnav Mohan <arnav.mohan@gmail.com>
2017-12-12 09:43:20 -0600

    Logical 2

--- complex.cpp
+++ complex.cpp
@@ -186,16 +186,16 @@ class complex
 	lhs = product;
     }
 
-    complex operator/(const complex &lhs, const complex &rhs){
+    complex operator/(const complex &rhs){
     // uses formula from http://mathworld.wolfram.com/ComplexDivision.html
-      double resulting_real = (real(&rhs)*real(&lhs) + imag(&rhs)*imag(&lhs))/(pow(real(&lhs),2) + pow(imag(&lhs),2));
-      double resulting_imag = (imag(&rhs)*real(&lhs) + real(&rhs)*imag(&lhs))/(pow(real(&lhs),2) + pow(imag(&lhs),2));
+      double resulting_real = (real(&rhs)*re + imag(&rhs)*im)/(pow(re,2) + pow(im,2));
+      double resulting_imag = (imag(&rhs)*re + real(&rhs)*im)/(pow(re,2) + pow(im,2));
       complex final = new complex(resulting_real, resulting_imag);
       return final;
       }
-    void operator/=(const complex &lhs, const complex &rhs){
-      complex final = &lhs / &rhs;
-      lhs = final;
+    void operator/=(const complex &rhs){
+      complex final = this / &rhs;
+      this = final;
     }
 
     /////////////////////
