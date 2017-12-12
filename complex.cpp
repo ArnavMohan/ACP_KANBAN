237d6147ea415e97a94ea10336171a3c7e8c97ce
Arnav Mohan <arnav.mohan@gmail.com>
2017-12-12 08:31:24 -0600

    Do it!

--- complex.cpp
+++ complex.cpp
@@ -1,1 +1,85 @@
+class complex 
+{
+    double re, im;
 
+    ////////////////
+    //constructors//
+    ////////////////
+
+    complex::complex() {re=0.0; im=0.0;}
+    complex::complex(double r, double i) {re=r; im=i;}
+    complex::complex(double r){re=r, im=0}
+
+    //////////////////	
+    //base functions//
+    //////////////////
+    
+    double real(const complex&);
+    double imag(const complex&);
+    double abs(const complex);    
+    double norm (const complex);
+    complex conj(const complex);
+    complex sqrt(const complex);
+
+    //////////////////
+    //trig functions//
+    //////////////////
+
+    complex sin(const complex);
+    complex cos(const complex);
+    complex tan(const complex);
+
+    complex cosh(const complex);
+    complex sinh(const complex);
+
+    complex atan(const complex);
+
+    ////////////////////////////
+    //exponentiation functions//
+    ////////////////////////////
+
+    complex pow(const complex b, const complex exp);
+    complex pow(double b, const complex exp);
+    complex pow(const complex b, double exp);
+    complex pow(const complex b, int exp);
+
+    complex exp(const complex);
+    
+    /////////////////////////
+    //logarithmic functions//
+    /////////////////////////
+
+    complex log(const complex);
+    complex log10(const complex);
+
+    /////////////////////
+    //angular functions//
+    /////////////////////
+
+    double polar(double mag, double ang = 0.0);
+    double arg(const complex);
+
+    ////////////////////////
+    //arithmetic operators//
+    ////////////////////////
+  
+    complex operator+(const complex &lhs, const complex &rhs);
+    void operator +=(const complex &lhs, const complex &rhs);
+
+    complex operator-(const complex &lhs, const complex &rhs);
+    void operator-=(const complex &lhs, const complex &rhs);
+
+    complex operator*(const complex &lhs, const complex &rhs);
+    void operator*=(const complex &lhs, const complex &rhs);
+
+    complex operator/(const complex &lhs, const complex &rhs);
+    void operator/=(const complex &lhs, const complex &rhs);
+
+    /////////////////////
+    //logical operators//
+    /////////////////////
+    
+    boolean operator==(const complex &lhs, const complex &rhs);
+    boolean operator!=(const complex &lhs, const complex &rhs);
+       
+};
