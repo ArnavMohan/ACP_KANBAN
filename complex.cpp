cc4f85988270489f82bd7dbb0b1beff1ce59be3d
Jear-Bear <jperlmutter1@gmail.com>
2017-12-12 09:05:22 -0600

    added more trig functions

--- complex.cpp
+++ complex.cpp
@@ -32,6 +32,36 @@ class complex
     complex cosh(const complex);
     complex sinh(const complex);
 
+    complex asin(const complex);
+    complex acos(const complex);
+    complex acos(const complex comp)
+    {
+        double negate = double(comp < 0);
+        double r = complex::real(complex::abs(comp));
+        double i = complex::imag(complex::abs(comp));
+        double ret = -0.0187293;
+        double iret = -0.0187293;
+        ret = ret * r;
+        iret = iret * i;
+        ret = ret + 0.0742610;
+        iret = iret + 0.0742610;
+        ret = ret * r;
+        iret = iret * i;
+        ret = ret - 0.2121144;
+        iret = iret - 0.2121144;
+        ret = ret * r;
+        iret = iret * i;
+        ret = ret + 1.5707288;
+        iret = iret + 1.5707288;
+        ret = ret * sqrt(1.0-r);
+        iret = iret * sqrt(1.0-i);
+        ret = ret - 2 * negate * ret;
+        iret = iret - 2 * negate * iret;
+        complex::real(comp) = ret;
+        complex::imag(comp) = iret;
+        return comp;
+    }
+    
     complex atan(const complex);
 
     ////////////////////////////
