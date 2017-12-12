129c2df5d327baa8f886d42eea1cf4bf8b59deac
Jear-Bear <jperlmutter1@gmail.com>
2017-12-12 09:08:58 -0600

    Jared pow

--- complex.cpp
+++ complex.cpp
@@ -76,7 +76,31 @@ class complex
     complex pow(double b, const complex exp);
     complex pow(const complex b, double exp);
     complex pow(const complex b, int exp);
-
+    complex pow (const complex b, int exp)
+    {
+        for (int i = 0; i < exp; i++)
+        {
+        complex::real(b) *= complex::real(b);
+        }
+        if (exp % 4 == 3)
+        {
+        complex::imag(b) *= -1;
+        }
+        else if (exp % 4 == 2)
+        {
+        complex::imag(b) = -1;
+        }
+        else if (exp % 4 == 1)
+        {
+        complex::imag(b) *= 1;
+        }
+        else if (exp % 4 == 0)
+        {
+        complex::imag(b) = 1;
+        }
+        return b;
+    }
+    
     complex exp(const complex);
     
     /////////////////////////
