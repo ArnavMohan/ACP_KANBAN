8392e2e00a99c36c6616ddc35c11e252ae8bec8e
Arnav Mohan <arnav.mohan@gmail.com>
2017-12-12 08:45:31 -0600

    Yep

--- complex.cpp
+++ complex.cpp
@@ -67,10 +67,10 @@ class complex
     void operator +=(const complex &lhs, const complex &rhs);
 
     complex operator-(const complex &lhs, const complex &rhs);
-    complex operator-(const complex &lhs, const complex &rhs)
+    complex operator-(const complex &rhs)
     {
-        double resulting_real = real(&lhs) - real(&rhs);
-        double resulting_imag = imag(&lhs) - imag(&rhs);
+        double resulting_real = re - real(&rhs);
+        double resulting_imag = im - imag(&rhs);
         //new complex type initialized with real and imaginary parts
         //return that complex number
         complex fin(resulting_real, resulting_imag);
