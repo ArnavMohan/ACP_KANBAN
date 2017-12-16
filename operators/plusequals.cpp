cee8942adbeaefbf632f97d4e1ee6f5c0cb3ae3e
rdchan <30301489+rdchan@users.noreply.github.com>
2017-12-15 22:40:25 -0600

    Update plusequals.cpp

--- operators/plusequals.cpp
+++ operators/plusequals.cpp
@@ -1,5 +1,7 @@
 #include "../complex.h"
-
+//Author: Abhijay
+//Operator: +=
+//Purpose: add complex numbers
 void operator +=(const complex &lhs, const complex &rhs)
 {
       complex final = &lhs + &rhs;
