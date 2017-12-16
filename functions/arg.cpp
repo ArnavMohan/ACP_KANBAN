583a459507f03338b6607dd077311eb24f4bf7a4
rdchan <30301489+rdchan@users.noreply.github.com>
2017-12-15 22:35:40 -0600

    author comment

--- functions/arg.cpp
+++ functions/arg.cpp
@@ -1,6 +1,8 @@
 #include "../complex.h>
 #include <math.h>
-
+//Author: Abhijay
+//Purpose: Return angle of a complex number
+//reliances: uses arctan from math.h
 double arg(const complex& n)
 {
 	double angle = atan(complex::imag(n)/complex::real(n));
